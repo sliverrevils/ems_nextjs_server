46425a720a9cba576a94a540299180db7b764bd7
Safonkin Nikita <sliverregulyar@gmail.com>
2024-09-07 16:24:06 +0300

    post fix complete

--- src/app/api/devises/route.ts
+++ src/app/api/devises/route.ts
@@ -19,22 +19,26 @@ export async function POST(req: Request) {
     const { deviseId, initDate } = (await req.json()) as IUser & { status: boolean };
     console.log(`➡️ DEVISE ID : ${deviseId}`);
     if (!deviseId) {
-        return;
+        return Response.json({ status: false, isCreated: false });
     }
     await connectDB();
 
     try {
-        // let devise = await Device.findOne({ deviseId: deviseId });
-        // console.log(`❗ DEVISE FIND : ${devise}`);
-        // if (!devise) {
-        //     devise = new Device({ deviseId: deviseId, initDate: Date.now() });
-        //     await devise.save();
-        // }
+        let devise = await Device.findOne({ deviseId: deviseId });
+        console.log(`❗ DEVISE FIND : ${devise}`);
+        if (devise === null) {
+            devise = new Device({ deviseId: deviseId, initDate: Date.now() });
+            await devise.save();
+            console.log(`✅ DEVISE CREATED : ${devise}`);
+            return Response.json({ devise, status: true, isCreated: true });
+        }
 
-        const result = await Device.updateOne({ deviseId: deviseId }, { $setOnInsert: { deviseId: deviseId, initDate: Date.now() } }, { upsert: true });
-        return Response.json({ result, status: true });
+        return Response.json({ status: false, isCreated: false });
+
+        // const result = await Device.updateOne({ deviseId: deviseId }, { $setOnInsert: { deviseId: deviseId, initDate: Date.now() } }, { upsert: true });
+        // return Response.json({ result, status: true });
     } catch (error) {
-        return Response.json({ status: true });
+        return Response.json({ status: false });
     }
 }
 
